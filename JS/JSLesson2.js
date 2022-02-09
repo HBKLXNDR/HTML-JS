3cf7f8be359030482d293cf87fa5c1c3181fa5ab
hbklksndr <hbklksndr@protonmail.com>
2022-02-10 01:15:25 +0200

    HW JS pt 3

--- JS/JSLesson2.js
+++ JS/JSLesson2.js
@@ -21,7 +21,7 @@ if (day => 1 && day <= 10) {
 } else if (day => 21 && day <= 31) {
     document.write("Third decade, my friend")
 }
-debugger
+
 
 // task 3
 
@@ -33,7 +33,65 @@ debugger
 // }
 
 let test = confirm('true or not?') ? 'true' : 'false';
-    console.log(test);
+console.log(test);
+
+//task 4
+let x = +prompt("input your nubmer")
+if (x !== 0) {
+    document.write("Correct")
+} else {
+    document.write("Incorrect")
+}
+;
+
+// last task
+let y = prompt("What is 'official' name of Javascript ?")
+if (y === "ECMAScript") {
+    document.write("Correct!")
+} else {
+    document.write("Don't know? ECMAScript!")
+}
+;
+// year task
+let extra = +prompt("What year do you choose?")
+if (extra % 4 === 0) {
+    document.write("Congrats! It is a leap year!")
+} else {
+    document.write("It is just an usual year, huh")
+}
+;
+// dayTask
+let ChooseYourDay = +prompt("Input number 1-7 to choose your day")
+switch (ChooseYourDay) {
+
+    case 1:
+        document.write(" Monday: come to work and cry");
+        break;
+
+    case 2:
+        document.write("Tuesday: come to work and cry again");
+        break;
+
+    case 3:
+        document.write("Wednesday: come to work and be sad");
+        break;
+
+    case 4:
+        document.write("Thursday: come to work and be OK");
+        break;
+
+    case 5:
+        document.write("Friday: come to work and be happy");
+        break;
+
+    case 6:
+        document.write("Saturday : try to wake up");
+        break;
+
+    case 7:
+        document.write("Sunday : closer to evening you cry again");
+        break;
+}
 
 
 // if else
