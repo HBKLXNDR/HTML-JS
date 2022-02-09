2a435e2c35f08a52f8c92599a5b1b3e1c717d609
hbklksndr <hbklksndr@protonmail.com>
2022-02-10 01:01:20 +0200

    HW JS pt 3

--- JS/JSHWLesson3.js
+++ JS/JSHWLesson3.js
@@ -45,45 +45,80 @@ for (let i = 0; i < someArray2.length; i++) {
     let someArray2Element = someArray2[i];
     console.log(someArray2Element);
 }
-let someArray3 = ['wow', true, NaN, null, false, 23, 33, 44,55,"woooow"];
+let someArray3 = ['wow', true, NaN, null, false, 23, 33, 44, 55, "woooow"];
 for (let i = 0; i < someArray3.length; i++) {
     let someArray3Element = someArray3[i];
     console.log(someArray3Element);
 }
-let someArray4 = ['wow', true, NaN, null, false, 23, 33, 44,55,"woooow"];
+let someArray4 = ['wow', true, NaN, null, false, 23, 33, 44, 55, "woooow"];
 for (let i = 0; i < someArray4.length; i++) {
-    if (typeof someArray4[i] === 'boolean'){
+    if (typeof someArray4[i] === 'boolean') {
         console.log(someArray4[i])
     }
 }
-let someArray5 = ['wow', true, NaN, null, false, 23, 33, 44,55,"woooow"];
+let someArray5 = ['wow', true, NaN, null, false, 23, 33, 44, 55, "woooow"];
 for (let i = 0; i < someArray5.length; i++) {
-    if (typeof someArray5[i] === 'string'){
+    if (typeof someArray5[i] === 'string') {
         console.log(someArray5[i])
     }
 }
-let someArray6 = ['wow', true, NaN, null, false, 23, 33, 44,55,"woooow"];
+let someArray6 = ['wow', true, NaN, null, false, 23, 33, 44, 55, "woooow"];
 for (let i = 0; i < someArray6.length; i++) {
-    if (typeof someArray6[i] === 'number'){
+    if (typeof someArray6[i] === 'number') {
         console.log(someArray6[i])
     }
 }
 
 let emptyArr = [];
-emptyArr[0]= 'Hello'
-emptyArr[1]= 'My friend'
-emptyArr[2]= 'do you believe'
-emptyArr[3]= 'in hot?'
-emptyArr[4]= 'I mean, hot chilli peppers, my friend'
+emptyArr[0] = 'Hello'
+emptyArr[1] = 'My friend'
+emptyArr[2] = 'do you believe'
+emptyArr[3] = 'in hot?'
+emptyArr[4] = 'I mean, hot chilli peppers, my friend'
 emptyArr[5] = 'Is it'
 emptyArr [6] = true
-emptyArr [7] ='or'
+emptyArr [7] = 'or'
 emptyArr [8] = false
 emptyArr [9] = 7777
 for (let i = 0; i < emptyArr.length; i++) {
     let emptyArrElement = emptyArr[i];
     console.log(emptyArrElement);
 }
+
+
+let someCycle;
+for (let i = 0; i < 10; i++) {
+    console.log('someCycle : ' + i + ' ');
+    document.write('someCycle : ' + i + ' ');
+}
+let someCycle1;
+for (let i = 0; i < 100; i++) {
+    console.log('someCycle1 : ' + i + ' ');
+    document.write('someCycle1 : ' + i + ' ');
+}
+let someCycle2;
+for (let i = 0; i < 100; i += 2) {
+    console.log('someCycle2 : ' + i + ' ');
+    document.write('someCycle2 : ' + i + ' ');
+}
+
+let someCycle3;
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        console.log('someCycle3 : ' + i + ' ');
+        document.write('someCycle3 : ' + i + ' ');
+    }
+}
+
+let someCycle4;
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 1) {
+        console.log('someCycle4 : ' + i + ' ');
+        document.write('someCycle4 : ' + i + ' ');
+    }
+}
+
+
 
 
 
