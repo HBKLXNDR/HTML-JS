9d688a42beaa8cc0aab4f2cd23d0865674e48dcf
hbklksndr <hbklksndr@protonmail.com>
2022-02-16 22:46:14 +0200

    HW JS pt 6

--- JS/JSHWpt6.js
+++ JS/JSHWpt6.js
@@ -1,5 +1,5 @@
 // task 1
-let someArray = 'hello world' ;
+let someArray = 'hello world';
 let someArray2 = 'lorem ipsum';
 let someArray3 = 'javascript is cool';
 console.log(someArray.length);
@@ -34,25 +34,38 @@ console.log(trim);
 
 let str = 'Каждый охотник желает знать';
 let stringToarray = (str) => {
-    return str.split(' ');}
+    return str.split(' ');
+}
 let someArray4 = stringToarray(str);
 console.log(someArray4);
 let add = someArray4.concat('где', 'сидит', 'фазан');
 console.log(add);
 
-
 // task 6
 
-
 let delete_characters = (n1, n2) => {
-    return str.substr(n1, n2)};
-console.log(delete_characters(0,7));
+    return str.substr(n1, n2)
+};
+console.log(delete_characters(0, 7));
 
 // task 7
 
-
-
 let str3 = 'HTML JavaScript PHP';
-let insert_dash = (n1) =>{
-    return str3.toUpperCase().replaceAll(n1 , '-')};
+let insert_dash = (n1) => {
+    return str3.toUpperCase().replaceAll(n1, '-')
+};
 console.log(insert_dash(' '));
+
+// task 8
+
+let FletterUpper = (someStr) => {
+    return someStr[0].toUpperCase() + someStr.slice(1);
+};
+console.log(FletterUpper("good bye, my friend"));
+
+// task 9
+
+let capitalize = (strStr) => {
+    return strStr.split(' ').map(word=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+};
+// I still do not understand this
