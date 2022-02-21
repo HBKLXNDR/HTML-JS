aca3e0a8020cefb95200bccc1a08f4058ba53786
markkravic <hbklksndr@protonmail.com>
2022-02-21 21:18:21 +0200

    HWJSpt7 full

---
+++ JS/CWJSpt7.js
@@ -0,0 +1,42 @@
+//
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+function KarlBenz (model,brand,year,max_speed,engine) {
+    this.model = model;
+    this.brand = brand;
+    this.year = year;
+    this.max_speed = max_speed;
+    this.engine = engine;
+
+    this.drive = function () {
+        console.log(`їдемо зі швидкістю ${this.max_speed} на годину`);
+    };
+    this.info = function () {
+        for (const key in this) {
+            if (typeof this[key] !== 'function') console.log(`${key} - ${this[key]}`)
+        }
+    };
+    this.newMaxSpeed = function (newSpeed) {
+        this.max_speed = this.max_speed + newSpeed;
+    };
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+    };
+    this.addDriver = function (driver) {
+        this.driver = driver;
+    };
+};
+let CarCreator = new KarlBenz('Marcedes-Benz', 'Germany', 2007, 150, 2.5);
+console.log(CarCreator);
+CarCreator.drive();
+CarCreator.info();
+CarCreator.newMaxSpeed(150);
+CarCreator.drive();
+CarCreator.changeYear(2019)
+CarCreator.info();
+CarCreator.addDriver('Marko')
+console.log(CarCreator);
